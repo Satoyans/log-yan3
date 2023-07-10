13076e2d8d27e4f7a9be998a527b0a99d6fe4544
satoyan <satoyan.discord@gmail.com>
2023-07-10 11:14:06 +0900

    sqlのテンプレートをアップデート

--- sql_setup/log-yan3_setup.sql
+++ sql_setup/log-yan3_setup.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- ホスト:                          127.0.0.1
--- サーバーのバージョン:                   8.0.32 - MySQL Community Server - GPL
+-- サーバーのバージョン:                   5.7.38-log - MySQL Community Server (GPL)
 -- サーバー OS:                      Win64
--- HeidiSQL バージョン:               12.4.0.6659
+-- HeidiSQL バージョン:               12.0.0.6468
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,158 +16,148 @@
 
 
 -- logyan のデータベース構造をダンプしています
-DROP DATABASE IF EXISTS `logyan`;
-CREATE DATABASE IF NOT EXISTS `logyan` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `logyan` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `logyan`;
 
 --  テーブル logyan.blockcontainer の構造をダンプしています
-DROP TABLE IF EXISTS `blockcontainer`;
 CREATE TABLE IF NOT EXISTS `blockcontainer` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `type` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `slot` int DEFAULT NULL,
-  `action` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `itemId` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `amount` int DEFAULT NULL,
+  `playerName` text CHARACTER SET utf8,
+  `type` text CHARACTER SET utf8,
+  `slot` int(11) DEFAULT NULL,
+  `action` text CHARACTER SET utf8,
+  `itemId` text CHARACTER SET utf8,
+  `amount` int(11) DEFAULT NULL,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.blockdestroy の構造をダンプしています
-DROP TABLE IF EXISTS `blockdestroy`;
 CREATE TABLE IF NOT EXISTS `blockdestroy` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockData` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
+  `blockName` text CHARACTER SET utf8,
+  `blockData` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.blockinteractedwith の構造をダンプしています
-DROP TABLE IF EXISTS `blockinteractedwith`;
 CREATE TABLE IF NOT EXISTS `blockinteractedwith` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockData` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
+  `blockName` text CHARACTER SET utf8,
+  `blockData` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.blockplace の構造をダンプしています
-DROP TABLE IF EXISTS `blockplace`;
 CREATE TABLE IF NOT EXISTS `blockplace` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockData` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
+  `blockName` text CHARACTER SET utf8,
+  `blockData` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.entitydie の構造をダンプしています
-DROP TABLE IF EXISTS `entitydie`;
 CREATE TABLE IF NOT EXISTS `entitydie` (
   `time` datetime DEFAULT NULL,
-  `attackerId` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `attackerName` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `victimId` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `victimName` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `cause` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `attackerId` text CHARACTER SET utf8,
+  `attackerName` text CHARACTER SET utf8,
+  `victimId` text CHARACTER SET utf8,
+  `victimName` text CHARACTER SET utf8,
+  `cause` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.getelytra の構造をダンプしています
-DROP TABLE IF EXISTS `getelytra`;
 CREATE TABLE IF NOT EXISTS `getelytra` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.itemthrow の構造をダンプしています
-DROP TABLE IF EXISTS `itemthrow`;
 CREATE TABLE IF NOT EXISTS `itemthrow` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `itemName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
+  `itemName` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.lightninghitblock の構造をダンプしています
-DROP TABLE IF EXISTS `lightninghitblock`;
 CREATE TABLE IF NOT EXISTS `lightninghitblock` (
   `time` datetime DEFAULT NULL,
-  `blockName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `blockData` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `blockName` text CHARACTER SET utf8,
+  `blockData` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.playerattack の構造をダンプしています
-DROP TABLE IF EXISTS `playerattack`;
 CREATE TABLE IF NOT EXISTS `playerattack` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `victimId` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `victimName` mediumtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text CHARACTER SET utf8,
+  `victimId` text CHARACTER SET utf8,
+  `victimName` text CHARACTER SET utf8,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
+  `dimension` text CHARACTER SET utf8
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- エクスポートするデータが選択されていません
 
 --  テーブル logyan.signblockplace の構造をダンプしています
-DROP TABLE IF EXISTS `signblockplace`;
 CREATE TABLE IF NOT EXISTS `signblockplace` (
   `time` datetime DEFAULT NULL,
-  `playerName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `id` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
-  `text` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `playerName` text,
+  `id` text,
+  `side` text,
+  `text` text,
   `x` float DEFAULT NULL,
   `y` float DEFAULT NULL,
   `z` float DEFAULT NULL,
-  `dimension` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `dimension` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- エクスポートするデータが選択されていません
 
