0a17a994702d1c2837301e35ddb66a47fabe8bd8
satoyan <satoyan.discord@gmail.com>
2023-08-09 10:45:56 +0900

    [fix]Changed to destroys connections created

--- src/sql.ts
+++ src/sql.ts
@@ -1,5 +1,5 @@
-import { events } from 'bdsx/event';
-import * as mysql from 'mysql';
+import { events } from "bdsx/event";
+import * as mysql from "mysql";
 import { config } from "dotenv";
 import * as path from "path";
 config({ path: path.join(__dirname, ".env") });
@@ -8,45 +8,55 @@ const sql_port = process.env.SQL_PORT;
 const sql_pass = process.env.SQL_PASSWORD;
 const sql_pass_db = process.env.SQL_DATABASE;
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    port: Number(sql_port),
-    password: sql_pass,
-    database: sql_pass_db
+	host: "localhost",
+	user: "root",
+	port: Number(sql_port),
+	password: sql_pass,
+	database: sql_pass_db,
 });
 
 async function query(queryText: string, func?: any) {
-    try {
-        let connection = await getConnection();
-        connection.query(queryText, func);
-    } catch (e) { console.error(e) };
+	try {
+		let connection = await getConnection();
+		connection.query(queryText, func);
+		connection.destroy();
+	} catch (e) {
+		console.error(e);
+	}
 }
 function queryAsync(queryText: string): Promise<any> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let connection = await getConnection();
-            connection.query(queryText, (err: mysql.MysqlError | null, result: any, field: mysql.FieldInfo[] | undefined) => {
-                if (err) reject(err);
-                resolve(result);
-            });
-        } catch (e) { console.error(e) };
-    })
+	return new Promise(async (resolve, reject) => {
+		try {
+			let connection = await getConnection();
+			connection.query(
+				queryText,
+				(
+					err: mysql.MysqlError | null,
+					result: any,
+					field: mysql.FieldInfo[] | undefined
+				) => {
+					if (err) reject(err);
+					resolve(result);
+				}
+			);
+			connection.destroy();
+		} catch (e) {
+			console.error(e);
+		}
+	});
 }
 
 function getConnection(): Promise<mysql.PoolConnection> {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) reject(err);
-            resolve(connection);
-        });
-    });
+	return new Promise((resolve, reject) => {
+		pool.getConnection((err, connection) => {
+			if (err) reject(err);
+			resolve(connection);
+		});
+	});
 }
 
 events.serverClose.on(() => {
-    pool.end
+	pool.end();
 });
 
-export {
-    query,
-    queryAsync
-}
+export { query, queryAsync };
